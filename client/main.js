f48aa5055deb60fa1c5e4a7cc73bcf6920fbd913
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2016-05-03 18:51:38 +0900

    Step 2

--- client/main.js
+++ client/main.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
 
 Meteor.startup(() => {
-  console.log("Hello, World!");
+  render(
+    <h1>Hello, Meteor React World!</h1>,
+    document.getElementById('render-root')
+  );
 });
